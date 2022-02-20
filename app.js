480987b7454bc3613f0cf7f4b25c63594be21e7b
alexhersfeldt <39616651+alexhersfeldt@users.noreply.github.com>
2022-02-20 11:54:30 +0100

    little update

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get("/time", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
 
-app.listen(8080, () => {
-    console.log("Server running on port", 8080);
+app.listen(process.env.PORT, () => {
+    console.log("Server running on port", process.env.PORT);
 })
