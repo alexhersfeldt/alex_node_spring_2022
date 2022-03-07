534348dbf97b1600361c3199335ff0c08e9b393b
alexhersfeldt <39616651+alexhersfeldt@users.noreply.github.com>
2022-03-07 09:47:40 +0100

    website running, just missing text

--- router/web-router.js
+++ router/web-router.js
@@ -2,27 +2,21 @@ const router = require("express").Router();
 const fs = require("fs");
 const path = require("path");
 
-//path.join(__dirname, "/public/components/nav.html\")
-function renderHtmlPage(htmlFile) {
-    const nav = fs.readFileSync("../public/components/nav.html").toString();
-    const header = fs.readFileSync("./public/components/header.html").toString();
-    const page = fs.readFileSync(`./public/page/${htmlFile}.html`).toString();
-    const footer = fs.readFileSync("./public/components/footer.html").toString();
-    return nav + header + page + footer;
-};
+
+
 
 const homePage = renderHtmlPage("home");
-/*const nodePage = renderHtmlPage("node");
-const npmPage = renderHtmlPage("npm");
-const nodemonPage = renderHtmlPage("nodemon");
-const expressPage = renderHtmlPage("express");
-const basicWebServerPage = renderHtmlPage("basicWebServer");
-*/
 
 router.get("/", (req, res) => res.send(homePage));
 router.get("/:pageName", (req, res) => {
-    const pageName = req.params.pageName;
-    const page = renderHtmlPage(pageName)
-    res.send(page);
+    res.send(renderHtmlPage(req.params.pageName));
 })
+
+function renderHtmlPage(htmlFile) {
+    const nav = fs.readFileSync("./public/components/nav.html").toString();
+    const header = fs.readFileSync("./public/components/header.html").toString();
+    const page = fs.readFileSync(`./public/pages/${htmlFile}.html`).toString();
+    const footer = fs.readFileSync("./public/components/footer.html").toString();
+    return nav + header + page + footer;
+};
 module.exports = router;
